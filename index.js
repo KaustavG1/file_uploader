4776a9363960bf2cf60393369eaed1ac73669c3c
KaustavG1 <mail2kg2@gmail.com>
2021-02-26 14:15:21 +0530

    Minor modifications

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const { response } = require('express');
 const fileUpload = require('express-fileupload');
 const { request } = require('http');
 
-<<<<<<< HEAD
 if(process.env.NODE_ENV === 'development') {
     var env = 'dev';
 } 
@@ -16,30 +15,18 @@ if(process.env.NODE_ENV === 'production') {
 
 const customEnv = require('custom-env').env(true);
 
-// app.use(fileUpload({ useTempFiles : true, tempFileDir : '/tmp/' }));
 app.use(fileUpload());
-// console.log(process.env.NODE_ENV);
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + "/index.html");
     console.log(process.env);
-=======
-app.use(fileUpload({ useTempFiles : true, tempFileDir : '/tmp/' }));
-
-app.get('/', (request, response) => {
-    response.sendFile(__dirname + "/index.html");
->>>>>>> 01adc819b82b7ae9cdf6c5679eac630de3461e22
 });
 
 app.post('/', (request, response) => {
     if(request.files) {
         const file = request.files.file;
         const fileName = file.name;
-<<<<<<< HEAD
         const uploadPath = path.resolve(__dirname, process.env.UPLOADS_FOLDER, fileName);
-=======
-        const uploadPath = path.resolve(__dirname, 'uploads', fileName);
->>>>>>> 01adc819b82b7ae9cdf6c5679eac630de3461e22
         file.mv(uploadPath, err => {
             if(err) {
                 response.send(err);
